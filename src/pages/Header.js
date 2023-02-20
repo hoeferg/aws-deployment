218d4c1d33f028254288b5d264b998a656b687ca
Gayle Hoefer <gaylehoefer@gmail.com>
2023-02-20 11:28:42 -0700

    added homepage

---
+++ src/pages/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function header() {
+    return (
+        <>
+            <h1> Gayle Hoefer Portfolio</h1>
+            <button>Projects</button>
+            <button>Resume</button>
+            <button>Contact Me</button>
+        </>
+    )
+}
---
+++ src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Homepage() {
+    return (
+        <>
+            <h1>I'm Gayle Hoefer</h1>
+            <p>I'm a full stack web developer who plays eleven instruments on the side. I'm looking to for a place where I can add my rhythm of coding.</p>
+            <p>I love to create new pieces of code that add to the jazz of life. View my projects, resume, and contact me page, or contact email me at gaylehoefer@gmail.com. </p>
+        </>
+    )
+}
